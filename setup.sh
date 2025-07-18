87027f34baf12c5abf19c29091402b4894938410
chhavi <dwivedichhavi19@gmail.com>
2025-07-19 01:25:29 +0530

    beh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/.streamlit/
 
-echo"\
-[server]
-headless=true
-enableCORS=false
-port=\$PORT
-">~/.streamlit/config.toml
+echo "[server]" >~/.streamlit/
+config.toml
+echo "headless=true" >> ~/.streamlit/
+config.toml
+echo "enableCORS=false" >>
+~/.streamlit/config.toml
+echo "port=$PORT" >> ~/.streamlit/config.toml
